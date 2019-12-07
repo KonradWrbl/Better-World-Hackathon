cf88f4788a4c338ff0d371e217f0f5284490ff45
KonradWrbl <160815@stud.prz.edu.pl>
2019-12-07 22:27:24 +0100

    hardInit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Additions from "./pages/Additions/Additions";
 import GlobalStyle from "./assets/styles/globalStyle";
 import theme from "./assets/styles/theme";
 import { ThemeProvider } from "styled-components";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHome, faChartPie, faLink, faCog, faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faHome, faChartPie, faLink, faCog, faChevronUp, faChevronDown)
+
 
 function App() {
   return (
