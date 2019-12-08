5dc762798fbcebd4d611559257863280049b26cb
Joanna Wytrzes <joannawytrzes@gmail.com>
2019-12-08 01:41:17 +0100

    Half of homepage

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import Additions from "./pages/Additions/Additions";
 import GlobalStyle from "./assets/styles/globalStyle";
 import theme from "./assets/styles/theme";
 import { ThemeProvider } from "styled-components";
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHome, faChartPie, faLink, faCog, faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faHome,
+  faChartPie,
+  faLink,
+  faCog,
+  faChevronUp,
+  faChevronDown
+} from "@fortawesome/free-solid-svg-icons";
 import BottomMenu from "./components/BottomMenu/BottomMenu";
 
-library.add(faHome, faChartPie, faLink, faCog, faChevronUp, faChevronDown)
-
+library.add(faHome, faChartPie, faLink, faCog, faChevronUp, faChevronDown);
 
 function App() {
   return (
@@ -24,7 +30,7 @@ function App() {
           <Route path="/additions">
             <Additions />
           </Route>
-        </Switch>      
+        </Switch>
       </Router>
       <BottomMenu />
     </ThemeProvider>
